db6115a9c5b1a97a3f7bbe94c9829e54ce5564df
Jonathan Feinberg <jonathf@gmail.com>
2021-12-23 10:59:12 +0100

    circle ci baseline

--- src/vroom/__init__.py
+++ src/vroom/__init__.py
@@ -1,10 +1,10 @@
 """Vehicle routing open-source optimization machine (VROOM)."""
 from .break_ import Break
-from .job import Job
+from .job import Job, JOB_TYPE
 from .location import Location
 from .time_window import TimeWindow
 from .vehicle import Vehicle
 
 from .input.forced_service import ForcedService
 from .input.input import Input
-from .input.vehicle_step import VehicleStep
+from .input.vehicle_step import VehicleStep, STEP_TYPE
