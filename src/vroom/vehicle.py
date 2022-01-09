a33d9ca3f1586e01fc06d2ff8ee30d7f9e394fee
Jonathan Feinberg <jonathf@gmail.com>
2022-01-09 13:38:09 +0100

    adding vehicle

--- src/vroom/vehicle.py
+++ src/vroom/vehicle.py
@@ -1,21 +1,62 @@
+from __future__ import annotations
 from typing import List, Optional, Sequence, Set, Union
+
+import numpy
 
 from .amount import Amount
 from .break_ import Break
-from .location import Location
-from .time_window import TimeWindow
 from .input.vehicle_step import VehicleStep
+from .location import Location, LocationCoordinates, LocationIndex
+from .time_window import TimeWindow
 
 from . import _vroom
+
+MAX_VAL = int(numpy.iinfo(numpy.uintp).max)
 
 
 class Vehicle(_vroom.Vehicle):
     """Vehicle for performing transport.
 
+    Args:
+        id:
+            Vehicle idenfifier number. Two vehicle can not have the same
+            identifier.
+        start:
+            The location where the vehicle starts at before any jobs are done.
+            If omitted, the vehicle will start at the first task it will be
+            assigned. If interger, value interpreted as an the column in
+            duration matrix. If pair of numbers, value interpreted as longitude
+            and latitude coordinates respectively.
+        end:
+            The location where the vehicle should end up after all jobs are
+            completed. If omitted, the vehicle will end at the last task it
+            will be assigned. If interger, value interpreted as an the column
+            in duration matrix. If pair of numbers, value interpreted as
+            longitude and latitude coordinates respectively.
+        profile:
+            The name of the profile this vehicle falls under.
+        capacity:
+            Array of intergers representing the capacity to carry different
+            goods.
+        skills:
+            Skills provided by this vehilcle needed to perform various tasks.
+        time_window:
+            The time window for when this vehicle is available for usage.
+        breaks:
+            Breaks this vehicle should take.
+        description:
+            Optional string descriping the vehicle.
+        speed_factor:
+            The speed factor for which this vehicle runs faster or slower than
+            the default.
+        max_tasks:
+            The maximum number of tasks this vehicle can perform.
+        steps:
+            Set of custom steps this vehicle should take.
+
     Examples:
-        >>> vehicle = Vehicle(1, end=1)
-        >>> vehicle
-        vroom.Vehicle(1, end=1, profile='car')
+        >>>  Vehicle(1, end=1)
+        vroom.Vehicle(1, end=1)
     """
 
     def __init__(
@@ -24,42 +65,73 @@ class Vehicle(_vroom.Vehicle):
         start: Union[None, Location, int, Sequence[float]] = None,
         end: Union[None, Location, int, Sequence[float]] = None,
         profile: str = "car",
-        capacity: Amount = Amount(),
+        capacity: Optional[Amount] = None,
         skills: Optional[Set[int]] = None,
         time_window: Optional[TimeWindow] = None,
         breaks: Sequence[Break] = (),
         description: str = "",
-        speed_factor: Optional[float] = None,
-        max_tasks: Optional[int] = None,
-        input_steps: Sequence[VehicleStep] = (),
+        speed_factor: float = 1.0,
+        max_tasks: int = MAX_VAL,
+        steps: Sequence[VehicleStep] = (),
     ) -> None:
-        kwargs = dict(
-            id=id,
-            start=start,
-            end=end,
-            profile=profile,
-            capacity=Amount(capacity),
-            skills=skills,
-            time_window=time_window,
-            breaks=breaks,
-            description=description,
-            speed_factor=speed_factor,
+        _vroom.Vehicle.__init__(
+            self,
+            id=int(id),
+            start=(None if start is None else Location(start)),
+            end=(None if end is None else Location(end)),
+            profile=str(profile),
+            capacity=(Amount([]) if capacity is None else capacity),
+            skills=(set([]) if skills is None else skills),
+            time_window=(TimeWindow() if time_window is None else time_window),
+            breaks=[Break(break_) for break_ in breaks],
+            description=str(description),
+            speed_factor=float(speed_factor),
             max_tasks=max_tasks,
-            input_steps=input_steps,
+            steps=[VehicleStep(step) for step in steps],
         )
-        kwargs = {key: value for key, value in kwargs.items() if value or key == "id"}
-        self._kwargs = kwargs.copy()
-        kwargs["start"] = None if start is None else Location(start)
-        kwargs["end"] = None if end is None else Location(end)
-        if "time_window" in kwargs:
-            kwargs["tw"] = kwargs.pop("time_window")
-        _vroom.Vehicle.__init__(self, **kwargs)
+        self._speed_factor = speed_factor
 
     def __repr__(self) -> str:
-        kwargs = {key: value for key, value in self._kwargs.items() if key == "id" or value}
-        id = kwargs.pop("id")
-        args = ", ".join(f"{key}={value!r}" for key, value in kwargs.items())
-        return f"vroom.{self.__class__.__name__}({id}, {args})"
+        args = [f"{self.id}"]
+        if self.start is not None:
+            if isinstance(self.start, LocationIndex):
+                args.append(f"start={self.start.index}")
+            elif isinstance(self.start, LocationCoordinates):
+                args.append(f"start={self.start.coords}")
+            else:
+                args.append(f"start={self.start}")
+        if self.end is not None:
+            if isinstance(self.end, LocationIndex):
+                args.append(f"end={self.end.index}")
+            elif isinstance(self.end, LocationCoordinates):
+                args.append(f"end={self.end.coords}")
+            else:
+                args.append(f"end={self.end}")
+
+        for name, default in [
+            ("profile", "car"),
+            ("capacity", Amount([])),
+            ("skills", set()),
+            ("time_window", TimeWindow()),
+            ("breaks", []),
+            ("description", ""),
+            ("speed_factor", 1.),
+            ("max_tasks", MAX_VAL),
+            ("steps", []),
+        ]:
+            attribute = getattr(self, name)
+            if attribute != default:
+                args.append(f"{name}={attribute!r}")
+
+        return f"vroom.{self.__class__.__name__}({', '.join(args)})"
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @id.setter
+    def id(self, value) -> None:
+        self._id = value
 
     @property
     def start(self) -> Optional[Location]:
@@ -87,11 +159,19 @@ class Vehicle(_vroom.Vehicle):
 
     @property
     def breaks(self) -> List[Break]:
-        return Break(self._break)
+        return [Break(break_) for break_ in self._breaks]
 
     @property
     def description(self) -> str:
         return self._description
+
+    @property
+    def speed_factor(self) -> float:
+        return self._speed_factor
+
+    @speed_factor.setter
+    def speed_factor(self, value: float) -> None:
+        self._speed_factor = value
 
     @property
     def max_tasks(self) -> str:
@@ -100,3 +180,9 @@ class Vehicle(_vroom.Vehicle):
     @property
     def steps(self) -> List[VehicleStep]:
         return [VehicleStep(step) for step in self._steps]
+
+    def has_same_locations(self, vehicle: Vehicle) -> bool:
+        return self._has_same_locations(vehicle)
+
+    def has_same_profile(self, vehicle: Vehicle) -> bool:
+        return self._has_same_profile(vehicle)
