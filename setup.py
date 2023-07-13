62d0dad2dd4f94202b402b0dfef54070c28ba910
Jonathan Feinberg <jonathf@gmail.com>
2023-07-13 12:49:05 +0200

    giving up on win, updating on linux

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if platform.system() == "Windows":
         "-DWIN32_LEAN_AND_MEAN",
         "-DASIO_STANDALONE",
         "-DUSE_PYTHON_BINDINGS",
-        "-DUSE_ROUTING=true"
+        "-DUSE_ROUTING=true",
     ]
     extra_link_args = []
 
@@ -39,7 +39,7 @@ else:  # anything *nix
         "-DNDEBUG",
         "-DUSE_PYTHON_BINDINGS",
         "-DUSE_ROUTING=true",
-        "-DUSE_LIBGLPK=true"
+        "-DUSE_LIBGLPK=true",
     ]
     extra_link_args = [
         "-lpthread",
