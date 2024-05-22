fd9ae44c3e9292e4a53b9fec6cb714162e53c6a2
Sebastiano Milardo <s.milardo@hotmail.it>
2024-05-22 15:38:39 +0200

    Update solution package classes

--- src/bind/solution/route.cpp
+++ src/bind/solution/route.cpp
@@ -8,25 +8,33 @@ void init_route(py::module_ &m) {
 
   py::class_<vroom::Route>(m, "Route")
       .def(py::init<>())
-      .def(py::init([](vroom::Id vehicle, std::vector<vroom::Step> &steps,
-                       vroom::Cost cost, vroom::Duration setup,
-                       vroom::Duration service, vroom::Duration duration,
-                       vroom::Duration waiting_time, vroom::Priority priority,
+      .def(py::init([](vroom::Id vehicle,
+                       std::vector<vroom::Step> &steps,
+                       vroom::UserCost cost,
+                       vroom::UserDuration duration,
+                       vroom::UserDistance distance,
+                       vroom::UserDuration setup,
+                       vroom::UserDuration service,
+                       vroom::UserDuration waiting_time,
+                       vroom::Priority priority,
                        const vroom::Amount &delivery,
-                       const vroom::Amount &pickup, const std::string &profile,
+                       const vroom::Amount &pickup,
+                       const std::string &profile,
                        const std::string &description,
-                       const vroom::Violations &violations) {
-        return new vroom::Route(vehicle, std::move(steps), cost, setup, service,
-                                duration, waiting_time, priority, delivery,
+                       vroom::Violations &violations) {
+        return new vroom::Route(vehicle, std::move(steps), cost, duration, distance,
+                                setup, service, waiting_time, priority, delivery,
                                 pickup, profile, description,
                                 std::move(violations));
       }))
+
       .def_readwrite("vehicle", &vroom::Route::vehicle)
       .def_readonly("steps", &vroom::Route::steps)
       .def_readwrite("cost", &vroom::Route::cost)
       .def_readwrite("setup", &vroom::Route::setup)
       .def_readwrite("service", &vroom::Route::service)
       .def_readwrite("duration", &vroom::Route::duration)
+      .def_readwrite("distance", &vroom::Route::distance)
       .def_readwrite("waiting_time", &vroom::Route::waiting_time)
       .def_readwrite("priority", &vroom::Route::priority)
       .def_readwrite("delivery", &vroom::Route::delivery)
