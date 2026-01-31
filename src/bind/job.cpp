182dc2c73ec73981e110854967aaeab6357dc0a7
Jonathan Feinberg <jonathf@gmail.com>
2026-01-31 15:42:40 +0100

    iterate

--- src/bind/job.cpp
+++ src/bind/job.cpp
@@ -9,40 +9,48 @@ namespace py = pybind11;
 void init_job(py::module_ &m) {
 
   py::class_<vroom::Job>(m, "Job")
-      .def(py::init<vroom::Id, vroom::Location &, vroom::Duration,
-                    vroom::Duration, vroom::Amount &, vroom::Amount &,
+      .def(py::init<vroom::Id, vroom::Location &, vroom::UserDuration,
+                    vroom::UserDuration, vroom::Amount &, vroom::Amount &,
                     vroom::Skills &, vroom::Priority,
-                    std::vector<vroom::TimeWindow> &, std::string &>(),
+                    std::vector<vroom::TimeWindow> &, std::string &, vroom::TypeToUserDurationMap &, vroom::TypeToUserDurationMap>(),
            "Regular one-stop job.", py::arg("id"), py::arg("location"),
-           py::arg("setup") = 0, py::arg("service") = 0,
+           py::arg("default_setup") = 0, py::arg("default_service") = 0,
            py::arg("delivery") = vroom::Amount(0),
            py::arg("pickup") = vroom::Amount(0),
            py::arg("skills") = vroom::Skills(), py::arg("priority") = 0,
            py::arg("tws") =
                std::vector<vroom::TimeWindow>(1, vroom::TimeWindow()),
-           py::arg("description") = "")
+           py::arg("description") = "",
+           py::arg("setup_per_type") = vroom::TypeToUserDurationMap(),
+           py::arg("service_per_type") = vroom::TypeToUserDurationMap())
       .def(py::init<vroom::Id, vroom::JOB_TYPE, vroom::Location &,
                     vroom::Duration, vroom::Duration, vroom::Amount &,
                     vroom::Skills &, vroom::Priority,
-                    std::vector<vroom::TimeWindow> &, std::string &>(),
+                    std::vector<vroom::TimeWindow> &, std::string &, vroom::TypeToUserDurationMap &, vroom::TypeToUserDurationMap &>(),
            "Pickup and delivery job.", py::arg("id"), py::arg("type"),
-           py::arg("location"), py::arg("setup") = 0, py::arg("service") = 0,
+           py::arg("location"), py::arg("default_setup") = 0, py::arg("default_service") = 0,
            py::arg("amount") = vroom::Amount(0),
            py::arg("skills") = vroom::Skills(), py::arg("priority") = 0,
            py::arg("tws") =
                std::vector<vroom::TimeWindow>(1, vroom::TimeWindow()),
-           py::arg("description") = "")
+           py::arg("description") = "",
+           py::arg("setup_per_type") = vroom::TypeToUserDurationMap(),
+           py::arg("service_per_type") = vroom::TypeToUserDurationMap())
       .def("index", &vroom::Job::index)
       .def("is_valid_start", &vroom::Job::is_valid_start)
       .def_readonly("_id", &vroom::Job::id)
       .def_readwrite("_location", &vroom::Job::location)
       .def_readonly("_type", &vroom::Job::type)
-      .def_readonly("_setup", &vroom::Job::setup)
-      .def_readonly("_service", &vroom::Job::service)
+      .def_readonly("_default_setup", &vroom::Job::default_setup)
+      .def_readonly("_default_service", &vroom::Job::default_service)
       .def_readonly("_delivery", &vroom::Job::delivery)
       .def_readonly("_pickup", &vroom::Job::pickup)
       .def_readonly("_skills", &vroom::Job::skills)
       .def_readonly("_priority", &vroom::Job::priority)
       .def_readonly("_time_windows", &vroom::Job::tws)
-      .def_readonly("_description", &vroom::Job::description);
+      .def_readonly("_description", &vroom::Job::description)
+      .def_readonly("_setup_per_type", &vroom::Job::setup_per_type)
+      .def_readonly("_service_per_type", &vroom::Job::service_per_type)
+      .def_readonly("_setups", &vroom::Job::setups)
+      .def_readonly("_services", &vroom::Job::services);
 }
